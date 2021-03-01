500e1157d46241e1b013f10141d418cf130563d4
= <ljbfif50@gmail.com>
2021-03-01 19:20:08 +0900

    백업

--- demo/demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -1,17 +1,51 @@
 package com.example.demo.service;
 
-import java.lang.reflect.Member;
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
 
 public class MemberService {
-	private final MemberRepository memberRepository = new MemberRepository();
+	
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
 	
 	/**
-	 *  회원가입 
+	 * 회원 가입
+	 * @param member
+	 * @return
 	 */
 	public Long join(Member member) {
-		memberRepository.findByName(member.getName());
+		
+		validateDuplicateMember(member); //중복회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		// 중복 회원 안됨
+		memberRepository.findByName(member.getName())
+		.ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+	
+	/**
+	 * 전체 회원 조회
+	 * @return
+	 */
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	/**
+	 * 아이디 하나 조회
+	 * @param memberId
+	 * @return
+	 */
+	public Optional<Member> findOne(Long memberId){
+		return memberRepository.findById(memberId);
 	}
 	
 }
