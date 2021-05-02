fc90718b814d4d26608bf18eb2d37d649a7c8f66
izagood <ljbfif50@gmail.com>
2021-05-02 17:29:07 +0900

    백업

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -25,5 +25,7 @@ public class MemberRepositoryTest {
 		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 //		위와 같은 상황일때 java.lang.IllegalStateException: Failed to load ApplicationContext 에러가 발생한다.
 //		Caused by: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'entityManagerFactory' defined in class path resource [org/springframework/boot/autoconfigure/orm/jpa/HibernateJpaConfiguration.class]: Invocation of init method failed; nested exception is org.hibernate.service.spi.ServiceException: Unable to create requested service [org.hibernate.engine.jdbc.env.spi.JdbcEnvironment]
+//		최초 H2 생성하고 나서 현재 프로젝트에 맞는 DB 생성해줘야 함 
+		
 	}
 }
